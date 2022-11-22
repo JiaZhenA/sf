e72ac2d6f779abe718a76fe8874f1c97aaa94e64
JiaZhen <JiaZhen1@protonmail.com>
2022-11-22 21:00:33 +0100

    pep8

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -20,7 +20,7 @@ def random_predict(number: int = 1) -> int:
 
     while True:
         count += 1
-        predict_number = np.random.randint(min_predict, max_predict + 1)  # предполагаемое число
+        predict_number = np.random.randint(min_predict, max_predict + 1)
         if number > predict_number:
             min_predict = predict_number + 1
             continue
@@ -42,8 +42,8 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    # np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))
 
     for number in random_array:
         count_ls.append(random_predict(number))
